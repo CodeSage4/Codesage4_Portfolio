cfbfd01ca6b256cdca4e3f15eacdbb571a8a7b69
CodeSage4 <bhaktavarunbollapragada@gmail.com>
2025-03-08 21:36:02 +0530

    troubleshooting6

--- api/hello.js
+++
@@ -1,4 +0,0 @@
-// Simple Vercel serverless function
-module.exports = function(req, res) {
-  res.status(200).json({ message: 'Hello from the API!' });
-} 
--- api/index.js
+++
@@ -1,4 +0,0 @@
-// Root API handler for Vercel
-module.exports = function(req, res) {
-  res.status(200).json({ message: 'API is working!' });
-} 
